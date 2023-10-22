a29f9e8f74a469c1259c840ee7f281c914d705f8
Krimelz <krimzida21@gmail.com>
2023-10-22 18:11:36 +0300

    test

--- open-gl-engine.cpp
+++ open-gl-engine.cpp
@@ -1,12 +1,40 @@
-﻿// open-gl-engine.cpp: определяет точку входа для приложения.
-//
-
-#include "open-gl-engine.h"
-
-using namespace std;
+﻿#include "open-gl-engine.h"
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	if (!glfwInit())
+	{
+		std::cerr << "glfw init error!" << std::endl;
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Open GL Engine", nullptr, nullptr);
+
+	if (!window)
+	{
+		std::cerr << "glfw create window error!" << std::endl;
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL())
+	{
+		std::cerr << "glad load error!" << std::endl;
+		return -1;
+	}
+
+	glClearColor(1.0, 1.0, 0.2, 1.0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
