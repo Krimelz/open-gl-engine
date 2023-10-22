a164ca83f58280d49653243a5d5e1740c7c817ab
Krimelz <krimzida21@gmail.com>
2023-10-22 17:30:38 +0300

    first commit

---
+++ open-gl-engine.cpp
@@ -0,0 +1,12 @@
+﻿// open-gl-engine.cpp: определяет точку входа для приложения.
+//
+
+#include "open-gl-engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
